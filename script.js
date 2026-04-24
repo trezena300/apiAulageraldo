343870a6a9c607799fc19bb951fbc2978a935917
Vanetrz <vanessa.trz.amorim@gmail.com>
2026-04-24 15:22:00 -0300

    projeto funcionando

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 
-async function  onepiecefruits(){
-    let response = await fetch ("https://api.api-onepiece.com/v2/fruits/en");
-    let data = await response.json ();
-    console.log("dados brutos:")
-    console.log(data)
-    //sorteia uma fruta
+async function onepiecefruits() {
 
+    const musica = document.getElementById("musicaFundo");
 
-let numeroAleatorio = Math.floor(Math.random() * data.length);
-let fruta = data[numeroAleatorio];
+    // toca a música (e evita erro se clicar várias vezes)
+    if (musica.paused) {
+        musica.play().catch(e => console.log("Erro ao tocar:", e));
+    }
 
+    let response = await fetch("https://api.api-onepiece.com/v2/fruits/en");
+    let data = await response.json();
 
-    console.log("fruits:")
-    console.log(data)
-    console.log(data[0])
-    console.log(fruta)
+    let numeroAleatorio = Math.floor(Math.random() * data.length);
+    let fruta = data[numeroAleatorio];
+
     document.getElementById("fruits").innerHTML = `
-
-
-    <h2>${fruta.name} </2h>
-    
-    <p> nomeJapones: ${fruta.roman_name} </p>
-    <p> categoria: ${fruta.type} </p>
-    <p> descricao: ${fruta.description}</p>
-
-    `
-    ;
+        <h2>${fruta.name}</h2>
+        <p>nomeJapones: ${fruta.roman_name}</p>
+        <p>categoria: ${fruta.type}</p>
+        <p>descricao: ${fruta.description}</p>
+    `;
 }
